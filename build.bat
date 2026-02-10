20d7badfae7f3c8cf4ac644a609d234867b49166
SF6Viewer User <user@sf6viewer.local>
2026-02-10 20:57:16 +0900

    Build: Switch to console mode (Remove --windowed)

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ echo Installing PyInstaller...
 pip install pyinstaller
 
 echo Building SF6 Viewer...
-pyinstaller --noconfirm --onedir --windowed --add-data "static;static" --name "SF6Viewer" main.py
+pyinstaller --noconfirm --onedir --noupx --add-data "static;static" --name "SF6Viewer" --exclude-module PyQt5 --exclude-module PyQt6 --exclude-module tkinter main.py
 
 echo Build complete!
 echo The executable is located in the 'dist' folder.
