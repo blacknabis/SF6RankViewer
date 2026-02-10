2c10c4b7d041b55b454aac7acaebe2c51a933af8
SF6Viewer User <user@sf6viewer.local>
2026-02-10 20:29:34 +0900

    Build: Switch to --onedir to resolve Windows Defender false positives

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ echo Installing PyInstaller...
 pip install pyinstaller
 
 echo Building SF6 Viewer...
-pyinstaller --noconfirm --onefile --windowed --add-data "static;static" --name "SF6Viewer" main.py
+pyinstaller --noconfirm --onedir --windowed --add-data "static;static" --name "SF6Viewer" main.py
 
 echo Build complete!
 echo The executable is located in the 'dist' folder.
